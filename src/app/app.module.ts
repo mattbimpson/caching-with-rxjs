4426338bbff4ff70e575a8767f579274cda2a3ed
Matt Bimpson <m.bimpson@gmail.com>
2020-07-20 12:36:34 -0500

    provide service, display data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductService } from './services/product-service';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,7 @@ import { ProductListComponent } from './components/product-list/product-list.com
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
